4457a413973b5ff061c0529be3d04c947419947e
ismailboughada2005-crypto <ismail.boughada.2005@gmail.com>
2026-06-27 18:59:41 +0100

    version 2.2

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\DoctorAppointmentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
